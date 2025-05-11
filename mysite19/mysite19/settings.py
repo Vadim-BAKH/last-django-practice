abfa2f429a33f8409066aaffd7aa5a9f093c7484
Vadim Bakhtiarov <vadima64@mail.ru>
2025-05-11 09:54:53 +0300

    some settings

--- mysite19/mysite19/settings.py
+++ mysite19/mysite19/settings.py
@@ -27,8 +27,8 @@ if SENTRY_DSN:
     )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_DIR = BASE_DIR / "database"
-DATABASE_DIR.mkdir(exist_ok=True)
+# DATABASE_DIR = BASE_DIR / "database"
+# DATABASE_DIR.mkdir(exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
